57deece6891d647381cd09168cfcaa7ec3790508
Crasun Jans <jpanthi@utexas.edu>
2024-05-17 22:52:35 -0500

    add experiment planning parameter info

--- cca_spot/src/cca_spot_node.cpp
+++ cca_spot/src/cca_spot_node.cpp
@@ -14,38 +14,52 @@ int main(int argc, char **argv)
     std::thread spinner_thread([&node]() { rclcpp::spin(node); });
     /*------------------------------------------------------------*/
     // Specify affordance screw
-    /* const Eigen::Vector3d aff_screw_axis(-1, 0, 0); // screw axis */
-    const Eigen::Vector3d aff_screw_axis(1, 0, 0); // screw axis
-    /* const Eigen::Vector3d aff_screw_axis(0, 1, 0);          // screw axis */
+    const Eigen::Vector3d aff_screw_axis(1, 0, 0); //  pulling_a_drawer only for visualization
+    /* const Eigen::Vector3d aff_screw_axis(1, 0, 0); //  pushing_a_drawer only for visualization */
+    /* const Eigen::Vector3d aff_screw_axis(0, 0, 1); // moving a stool */
+    /* const Eigen::Vector3d aff_screw_axis(1, 0, 0); // valve_turn_case_3 and 4 */
+    /* const Eigen::Vector3d aff_screw_axis(0, 1, 0);          // valve_turn_case_1 and 2 */
     const Eigen::Vector3d aff_screw_axis_location(0, 0, 0); // location vector
     /*------------------------------------------------------------*/
 
     /*------------------------------------------------------------*/
     // Set affordance goal
-    const double aff_goal = (1.75) * M_PI; // Code
-    /* const double aff_goal = (1.0 / 2.0) * M_PI; // Code */
-    /* const double aff_goal = (1.0) * M_PI; // Code */
-    /* const double aff_goal = (7.0 / 3.0) * M_PI; // Code */
-    /* Eigen::VectorXd sec_goal(1); */
-    Eigen::VectorXd sec_goal(2);
-    /* Eigen::VectorXd sec_goal(3); */
-    /* Eigen::VectorXd sec_goal(4); */
-    sec_goal[0] = 0.0;
+    const double aff_goal = 0.29; // pulling_a_drawer
+    /* const double aff_goal = 0.1999; // pushing_a_drawer */
+    /* const double aff_goal = (1.0 / 2.0) * M_PI; // moving_a_stool */
+    /* const double aff_goal = (7.0 / 3.0) * M_PI; // valve_turn_case_4 */
+    /* const double aff_goal = (1.75) * M_PI; // valve_turn_case_3 */
+    /* const double aff_goal = (1.0 / 4.0) * M_PI; // valve_turn_case_2 */
+    /* const double aff_goal = (1.0 / 2.0) * M_PI; // valve_turn_case_1 */
+    Eigen::VectorXd sec_goal(1); // pushing_a_drawer
+    /* Eigen::VectorXd sec_goal(1); // pulling_a_drawer */
+    /* Eigen::VectorXd sec_goal(1); // moving_a_stool */
+    /* Eigen::VectorXd sec_goal(1); // valve_turn_case_4 */
+    /* Eigen::VectorXd sec_goal(2); // valve_turn_case_3 */
+    /* Eigen::VectorXd sec_goal(1); // valve_turn_case_2 */
+    /* Eigen::VectorXd sec_goal(2); // valve_turn_case_1 */
+    /* sec_goal[0] = 0.0; */
     /* sec_goal[1] = 0.0; */
     /* sec_goal[2] = 0.0; */
     /* sec_goal[3] = aff_goal; */
-    sec_goal[1] = aff_goal;
-    /* sec_goal[0] = aff_goal; */
+    /* sec_goal[1] = aff_goal; */
+    sec_goal[0] = aff_goal;
     std::cout << "Here is the secondary goal: \n" << sec_goal << std::endl;
     /*------------------------------------------------------------*/
 
     /*------------------------------------------------------------*/
     // Optionally set planner parameters
-    const double aff_step = 0.2;
-    /* const int gripper_control_par_tau = 1; */
-    const int gripper_control_par_tau = 2;
-    /* const int gripper_control_par_tau = 3; */
-    /* const int gripper_control_par_tau = 4; */
+    const double aff_step = 0.05; // pulling_a_drawer
+    /* const double aff_step = 0.05; // pushing_a_drawer */
+    /* const double aff_step = 0.15; // moving_a_stool */
+    /* const double aff_step = 0.2;           // valve_turn experiments */
+    const int gripper_control_par_tau = 1; // pulling_a_drawer
+    /* const int gripper_control_par_tau = 1; // pushing_a_drawer */
+    /* const int gripper_control_par_tau = 1; // moving_a_stool */
+    /* const int gripper_control_par_tau = 1; // valve_turn_case_4 */
+    /* const int gripper_control_par_tau = 2; // valve_turn_case_3 */
+    /* const int gripper_control_par_tau = 1; // valve_turn_case_2 */
+    /* const int gripper_control_par_tau = 2; // valve_turn_case_1 */
     const double accuracy = 10.0 / 100.0;
     /*------------------------------------------------------------*/
 
